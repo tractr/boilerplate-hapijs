f8a38915b5809edc1a937fb96b224b1562984707
Edouard Demotes-Mainard <e.demotes@gmail.com>
2019-02-26 14:33:59 -0500

    Add models.md

---
+++ hapify/MODELS.md.js
@@ -0,0 +1,37 @@
+let md = `# Models definition\n\n`;
+
+const properties = [
+    "primary", "unique", "label", "nullable", "multiple", "important",
+    "searchable", "sortable", "isPrivate", "internal", "restricted", "ownership"
+];
+
+for (const model of models) {
+    md += `## ${model.names.wordsUpper}\n\n`;
+    md += `### Fields\n\n`;
+    for (const field of model.fields.list) {
+        md += `- ${field.names.wordsUpper}`;
+        md += ` [*${field.type}`;
+        if (field.type !== 'entity' && field.subtype) {
+            md += `:${field.subtype}`;
+        }
+        if (field.type === 'entity' && field.model) {
+            md += `:${field.model.names.upperCamel}`;
+        }
+        md += `*]`;
+        const p = properties.filter(p => !!field[p]);
+        if (p.length) {
+            md += `: ${p.join(', ')}`;
+        }
+        md += `\n`;
+    }
+    md += `\n`;
+    if (model.dependencies.list.length) {
+        md += `### Dependencies\n\n`;
+        md += `${model.dependencies.list.map(d => d.names.wordsUpper).join(', ')}\n\n`;
+    }
+}
+
+//--------------------------------------------------
+//  Output
+//--------------------------------------------------
+return md;
