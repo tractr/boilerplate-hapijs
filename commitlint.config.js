b05d527cd8578ab8449e5d96e9a8d42f7c495d31
Edouard Demotes-Mainard <e.demotes@gmail.com>
2020-05-11 23:40:51 -0400

    fix(commitlint): fix commitlint.config.js

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,5 @@
 'use strict';
 
-const types = ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'release', 'revert', 'style', 'test'];
-
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [2, 'always', types],
-  }
+	extends: ['@commitlint/config-conventional']
 };
